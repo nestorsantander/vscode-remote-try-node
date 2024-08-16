e6da820759db032c3ab7fb2628800d5178f91ecf
nestorsantander <136125952+nestorsantander@users.noreply.github.com>
2024-08-16 20:58:02 +0000

    Elimino test de app server

--- server.test.js
+++ server.test.js
@@ -4,10 +4,10 @@ test('test: calcularCuadrado', () => {
   expect(calcularCuadrado(3)).toBe(9);
 });
 
-test('test: appWeb', () => {  
+ /*  test('test: appWeb', () => {  
   app.get('/?numero=3', (req, res)=>{
     const numero = parseFloat(req.query.numero);
     const resultado = calcularCuadrado(numero);
     expect(res.send(`La potencia cuadrada de ${numero} es ${resultado}`)).toBe('La potencia cuadrada de 3 es 9');
   });
-});
+});*/
